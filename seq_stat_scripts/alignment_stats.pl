11200566751dafbe634f73f779ca5a6ce0d05e5d
tlane13 <tlane13utk@gmail.com>
2015-06-12 10:41:53 -0400

    Polished code.

--- seq_stat_scripts/alignment_stats.pl
+++ seq_stat_scripts/alignment_stats.pl
@@ -2,7 +2,10 @@
 
 use strict;
 use Bio::SeqIO;
-use Bio::Tools::Run::StandAloneBlast;
+BEGIN { $ENV{CLUSTALDIR} = '/lustre/projects/staton/software/clustalw-2.1-linux-x86_64-libcppstatic/' }
+use Bio::Tools::Run::Alignment::Clustalw;
+use Bio::SimpleAlign;
+use Bio::AlignIO;
  
 # Check for input
 if($ARGV[0] eq ""){
@@ -13,6 +16,7 @@ if($ARGV[0] eq ""){
 my $fasta = $ARGV[0];
 my %sequences;
 my $seqio;
+my @seq_array;
 
 # load fasta file into BioPerl seqio object
 $seqio = Bio::SeqIO->new(-file => $fasta, -format => "fasta");
@@ -20,7 +24,21 @@ while(my$seqobj = $seqio->next_seq) {
 	my $id  = $seqobj->display_id;
 	my $seq = $seqobj->seq;
 	$sequences{$id} = $seqobj;
+	# add seqio object to an array
+	push(@seq_array, $sequences{$id});
 }
 
-#my $factory = Bio::Tools::Run::StandAloneBlast->new(-outfile => 'bl2seq.out');
-#my $bl2seq_report = $factory->bl2seq($sequences{"Liriodendron_tulipifera_10132014_comp2_c0_seq1"}, $sequences{"Liriodendron_tulipifera_10132014_comp0_c0_seq1"});
+# run clustalw
+my $factory = Bio::Tools::Run::Alignment::Clustalw->new(-matrix => 'BLOSUM', -quiet => '1');
+my $ktuple = 3;
+$factory->ktuple($ktuple);  # change the parameter before executing
+my $seq_array_ref = \@seq_array;
+my $aln = $factory->align($seq_array_ref);
+
+# print results
+print $seq_array[0]->id();
+print "\t";
+print $seq_array[1]->id();
+print "\t";
+print $aln->percentage_identity();
+print "\n";
